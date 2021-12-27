0128d35a53d4528970cf10bacaedefc32d1d72f9
qiaoyuwen <qiao.yuwen@foundbyte.com>
2021-12-27 14:09:18 +0800

    feat: add event, event driver

--- packages/utils/src/event-driver.ts
+++ packages/utils/src/event-driver.ts
@@ -1,6 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ISubscriber } from '.';
-import { Event, ICustomEvent } from './event';
+import { Event, ICustomEvent, ICustomEventClass } from './event';
+
+export const EVENTS_SYMBOL = Symbol('EVENTS_SYMBOL');
+export const EVENTS_ONCE_SYMBOL = Symbol('EVENTS_ONCE_SYMBOL');
+export const EVENTS_BATCH_SYMBOL = Symbol('EVENTS_BATCH_SYMBOL');
+export const DRIVER_INSTANCES_SYMBOL = Symbol('DRIVER_INSTANCES_SYMBOL');
 
 export type EventContainer = Window | HTMLElement | Document;
 
@@ -12,7 +17,6 @@ export type EventOptions =
       EventListenerOptions & {
         mode?: 'onlyOne' | 'onlyParent' | 'onlyChild';
       });
-
 export interface IEventDriver {
   container: EventDriverContainer;
   contentWindow: Window;
@@ -62,10 +66,20 @@ export interface IEventDriver {
   batchRemoveEventListener(type: any, listener: any, options: any): void;
 }
 
+export interface IEventDriverClass<T> {
+  new (engine: T, context?: any): IEventDriver;
+}
+
+export interface IEventEffect<T> {
+  (engine: T): void;
+}
+
+const isOnlyMode = (mode: string) => mode === 'onlyOne' || mode === 'onlyChild' || mode === 'onlyParent';
+
 /**
  * 事件驱动器基类
  */
-export class EventDriver<Engine extends Event = Event, Context = any> /* implements IEventDriver */ {
+export class EventDriver<Engine extends Event = Event, Context = any> {
   public engine: Engine;
   public container: EventDriverContainer = document;
   public contentWindow: Window = window;
@@ -82,5 +96,147 @@ export class EventDriver<Engine extends Event = Event, Context = any> /* impleme
 
   public subscribe<T extends ICustomEvent<any, any> = any>(subscriber: ISubscriber<T>): void {
     this.engine.subscribe(subscriber);
+  }
+
+  public subscribeTo<T extends ICustomEventClass>(type: T, subscriber: ISubscriber<InstanceType<T>>) {
+    return this.engine.subscribeTo(type, subscriber);
+  }
+
+  public subscribeWith<T extends ICustomEvent = ICustomEvent>(type: string | string[], subscriber: ISubscriber<T>) {
+    return this.engine.subscribeWith(type, subscriber);
+  }
+
+  public attach(container: EventDriverContainer) {
+    console.error('attach must implement.');
+  }
+
+  public detach(container: EventDriverContainer) {
+    console.error('detach must implement.');
+  }
+
+  public eventTarget(type: string) {
+    if (type === 'resize' || type === 'scroll') {
+      if (this.container === this.contentWindow?.document) {
+        return this.contentWindow;
+      }
+    }
+    return this.container;
+  }
+
+  public addEventListener<K extends keyof HTMLElementEventMap>(
+    type: K,
+    listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any,
+    options?: boolean | EventOptions,
+  ): void;
+  public addEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventOptions,
+  ): void;
+  public addEventListener(type: any, listener: any, options: any) {
+    const target = this.eventTarget(type);
+    if (isOnlyMode(options?.mode)) {
+      target[EVENTS_ONCE_SYMBOL] = target[EVENTS_ONCE_SYMBOL] || {};
+      const constructor = this.constructor;
+      constructor[EVENTS_ONCE_SYMBOL] = target[EVENTS_ONCE_SYMBOL] || {};
+      const handler = target[EVENTS_ONCE_SYMBOL][type];
+      const container = constructor[EVENTS_ONCE_SYMBOL][type];
+      if (!handler) {
+        if (container) {
+          if (options?.mode === 'onlyChild') {
+            if (container.contains(target)) {
+              container.removeEventListener(type, container[EVENTS_ONCE_SYMBOL][type], options);
+            }
+            delete container[EVENTS_ONCE_SYMBOL][type];
+          } else if (options?.mode === 'onlyParent') {
+            if (container.contains(target)) {
+              return;
+            }
+          }
+        }
+        target.addEventListener(type, listener, options);
+        target[EVENTS_ONCE_SYMBOL][type] = listener;
+        constructor[EVENTS_ONCE_SYMBOL][type] = target;
+      }
+    } else {
+      target[EVENTS_SYMBOL] = target[EVENTS_SYMBOL] || {};
+      target[EVENTS_SYMBOL][type] = target[EVENTS_SYMBOL][type] || {};
+      if (!target[EVENTS_SYMBOL][type]?.get?.(listener)) {
+        target.addEventListener(type, listener, options);
+        target[EVENTS_SYMBOL][type]?.set?.(listener, true);
+      }
+    }
+  }
+
+  public removeEventListener<K extends keyof HTMLElementEventMap>(
+    type: K,
+    listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any,
+    options?: boolean | EventOptions,
+  ): void;
+  public removeEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventOptions,
+  ): void;
+  public removeEventListener(type: any, listener: any, options: any) {
+    const target = this.eventTarget(type);
+    if (isOnlyMode(options?.mode)) {
+      const constructor = this.constructor;
+      constructor[EVENTS_ONCE_SYMBOL] = constructor[EVENTS_ONCE_SYMBOL] || {};
+      target[EVENTS_ONCE_SYMBOL] = target[EVENTS_ONCE_SYMBOL] || {};
+      delete constructor[EVENTS_ONCE_SYMBOL][type];
+      delete target[EVENTS_ONCE_SYMBOL][type];
+      target.removeEventListener(type, listener, options);
+    } else {
+      target[EVENTS_SYMBOL] = target[EVENTS_SYMBOL] || {};
+      target[EVENTS_SYMBOL][type] = target[EVENTS_SYMBOL][type] || {};
+      target[EVENTS_SYMBOL][type]?.delete?.(listener);
+      target.removeEventListener(type, listener, options);
+    }
+  }
+
+  public batchAddEventListener<K extends keyof HTMLElementEventMap>(
+    type: K,
+    listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any,
+    options?: boolean | EventOptions,
+  ): void;
+  public batchAddEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventOptions,
+  ): void;
+  public batchAddEventListener(type: any, listener: any, options?: any) {
+    this.engine[DRIVER_INSTANCES_SYMBOL] = this.engine[DRIVER_INSTANCES_SYMBOL] || [];
+    if (!this.engine[DRIVER_INSTANCES_SYMBOL].includes(this)) {
+      this.engine[DRIVER_INSTANCES_SYMBOL].push(this);
+    }
+    this.engine[DRIVER_INSTANCES_SYMBOL].forEach((driver) => {
+      const target = driver.eventTarget(type);
+      target[EVENTS_BATCH_SYMBOL] = target[EVENTS_BATCH_SYMBOL] || {};
+      if (!target[EVENTS_BATCH_SYMBOL][type]) {
+        target.addEventListener(type, listener, options);
+        target[EVENTS_BATCH_SYMBOL][type] = listener;
+      }
+    });
+  }
+
+  public batchRemoveEventListener<K extends keyof HTMLElementEventMap>(
+    type: K,
+    listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any,
+    options?: boolean | EventOptions,
+  ): void;
+  public batchRemoveEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventOptions,
+  ): void;
+  public batchRemoveEventListener(type: any, listener: any, options: any) {
+    this.engine[DRIVER_INSTANCES_SYMBOL] = this.engine[DRIVER_INSTANCES_SYMBOL] || [];
+    this.engine[DRIVER_INSTANCES_SYMBOL].forEach((driver) => {
+      const target = driver.eventTarget(type);
+      target[EVENTS_BATCH_SYMBOL] = target[EVENTS_BATCH_SYMBOL] || {};
+      target.removeEventListener(type, listener, options);
+      delete target[EVENTS_BATCH_SYMBOL][type];
+    });
   }
 }
--- packages/utils/src/event.ts
+++ packages/utils/src/event.ts
@@ -1,5 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Subscribable } from '.';
+import { isArr, isWindow, Subscribable } from '.';
+import {
+  DRIVER_INSTANCES_SYMBOL,
+  EventContainer,
+  EVENTS_BATCH_SYMBOL,
+  EVENTS_ONCE_SYMBOL,
+  EVENTS_SYMBOL,
+  IEventDriverClass,
+  IEventEffect,
+} from './event-driver';
+import { ISubscriber } from './subscribable';
+
+const ATTACHED_SYMBOL = Symbol('ATTACHED_SYMBOL');
 
 export interface ICustomEvent<EventData = any, EventContext = any> {
   type: string;
@@ -7,7 +19,110 @@ export interface ICustomEvent<EventData = any, EventContext = any> {
   context?: EventContext;
 }
 
+export interface ICustomEventClass {
+  new (...args: any[]): any;
+}
+
+export interface IEventProps<T = Event> {
+  drivers?: IEventDriverClass<T>[];
+  effects?: IEventEffect<T>[];
+}
+
 /**
  * 事件引擎
  */
-export class Event extends Subscribable<ICustomEvent> {}
+export class Event extends Subscribable<ICustomEvent> {
+  private _drivers: IEventDriverClass<any>[] = [];
+  private _containers: EventContainer[] = [];
+
+  public constructor(props?: IEventProps) {
+    super();
+
+    if (isArr(props?.effects)) {
+      props.effects.forEach((plugin) => {
+        plugin(this);
+      });
+    }
+
+    if (isArr(props?.drivers)) {
+      this._drivers = props.drivers;
+    }
+  }
+
+  public subscribeTo<T extends ICustomEventClass>(type: T, subscriber: ISubscriber<InstanceType<T>>) {
+    return this.subscribe((event) => {
+      if (type && event instanceof type) {
+        return subscriber(event);
+      }
+    });
+  }
+
+  public subscribeWith<T extends ICustomEvent = ICustomEvent>(type: string | string[], subscriber: ISubscriber<T>) {
+    return this.subscribe((event) => {
+      if (isArr(type)) {
+        if (type.includes(event?.type)) {
+          return subscriber(event);
+        }
+      } else {
+        if (type && type === event?.type) {
+          return subscriber(event);
+        }
+      }
+    });
+  }
+
+  public attachEvents(container: EventContainer, contentWindow: Window = window, context?: any) {
+    if (!container) {
+      return;
+    }
+    if (isWindow(container)) {
+      return this.attachEvents(container.document, container, context);
+    }
+    if (container[ATTACHED_SYMBOL]) {
+      return;
+    }
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    container[ATTACHED_SYMBOL] = this._drivers.map((EventDriverClass) => {
+      const driver = new EventDriverClass(this, context);
+      driver.container = container;
+      driver.contentWindow = contentWindow;
+      driver.attach(container);
+      return driver;
+    });
+    if (!this._containers.includes(container)) {
+      this._containers.push(container);
+    }
+  }
+
+  public detachEvents(container?: EventContainer) {
+    if (!container) {
+      this._containers.forEach((c) => {
+        this.detachEvents(c);
+      });
+      return;
+    }
+    if (isWindow(container)) {
+      return this.detachEvents(container.document);
+    }
+    if (!container[ATTACHED_SYMBOL]) {
+      return;
+    }
+    container[ATTACHED_SYMBOL].forEach((driver) => {
+      driver.detach(container);
+    });
+
+    this[DRIVER_INSTANCES_SYMBOL] = this[DRIVER_INSTANCES_SYMBOL] || [];
+    this[DRIVER_INSTANCES_SYMBOL] = this[DRIVER_INSTANCES_SYMBOL].reduce((drivers, driver) => {
+      if (driver.container === container) {
+        driver.detach(container);
+        return drivers;
+      }
+      return drivers.concat(driver);
+    }, []);
+    this._containers = this._containers.filter((item) => item !== container);
+    delete container[ATTACHED_SYMBOL];
+    delete container[EVENTS_SYMBOL];
+    delete container[EVENTS_ONCE_SYMBOL];
+    delete container[EVENTS_BATCH_SYMBOL];
+  }
+}
