886d7e53d7183601cbe8690491f5df658b625187
qiaoyuwen <543684504@qq.com>
2021-12-28 14:23:26 +0800

    feat: tsconfig

--- packages/docs/docs/react-hooks/array/index.tsx
+++ packages/docs/docs/react-hooks/array/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import { FunctionComponent } from 'react';
 import { Space, Button, List } from 'antd';
 import 'antd/dist/antd.css';
 import { useArray } from '@qiaoyuwen-core-next/react-hooks';
