c39fbdff61aef197b4c5711c20a51e345c6487dc
qiaoyuwen <543684504@qq.com>
2022-02-08 16:39:09 +0800

    feat: init designer-playground

---
+++ packages/designer-playground/src/widgets/SchemaEditorWidget.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {
+  transformToSchema,
+  transformToTreeNode,
+} from '@designable/formily-transformer'
+import { TreeNode, ITreeNode } from '@qiaoyuwen-core-next/designer-core'
+import { MonacoInput } from '@designable/react-settings-form'
+
+export interface ISchemaEditorWidgetProps {
+  tree: TreeNode
+  onChange?: (tree: ITreeNode) => void
+}
+
+const TempMonacoInput = MonacoInput as any;
+
+export const SchemaEditorWidget: React.FC<ISchemaEditorWidgetProps> = (
+  props
+) => {
+  return (
+    <TempMonacoInput
+      {...props}
+      value={JSON.stringify(transformToSchema(props.tree as any), null, 2)}
+      onChange={(value) => {
+        props.onChange?.(transformToTreeNode(JSON.parse(value)) as any)
+      }}
+      language="json"
+    />
+  )
+}
