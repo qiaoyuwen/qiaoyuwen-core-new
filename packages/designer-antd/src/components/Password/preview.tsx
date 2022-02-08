0a59fe40bf6c0e422c1523efe990d5ae7e616a1c
qiaoyuwen <543684504@qq.com>
2022-02-08 15:22:50 +0800

    feat: init designer-antd

---
+++ packages/designer-antd/src/components/Password/preview.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Password as FormilyPassword } from '@formily/antd'
+import { createBehavior, createResource } from '@qiaoyuwen-core-next/designer-core'
+import { DnFC } from '@qiaoyuwen-core-next/designer-react'
+import { createFieldSchema } from '../Field'
+import { AllSchemas } from '../../schemas'
+import { AllLocales } from '../../locales'
+
+export const Password: DnFC<React.ComponentProps<typeof FormilyPassword>> =
+  FormilyPassword
+
+Password.Behavior = createBehavior({
+  name: 'Password',
+  extends: ['Field'],
+  selector: (node) => node.props['x-component'] === 'Password',
+  designerProps: {
+    propsSchema: createFieldSchema(AllSchemas.Password),
+  },
+  designerLocales: AllLocales.Password,
+})
+
+Password.Resource = createResource({
+  icon: 'PasswordSource',
+  elements: [
+    {
+      componentName: 'Field',
+      props: {
+        title: 'Password',
+        'x-decorator': 'FormItem',
+        'x-component': 'Password',
+      },
+    },
+  ],
+})
