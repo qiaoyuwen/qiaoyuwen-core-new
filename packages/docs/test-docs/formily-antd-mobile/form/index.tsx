2aad8f15712daa8a549b08e9f5d5bb3b4125cf42
qiaoyuwen <543684504@qq.com>
2022-03-10 16:26:31 +0800

    feat: update lock file

--- packages/docs/test-docs/formily-antd-mobile/form/index.tsx
+++ packages/docs/test-docs/formily-antd-mobile/form/index.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable @rushstack/typedef-var */
 import { FunctionComponent } from 'react';
-import { Form, FormItem, Input } from '@qiaoyuwen-core-next/formily-antd-mobile';
+import { Form, FormItem } from '@qiaoyuwen-core-next/formily-antd-mobile';
 import { createForm } from '@formily/core';
 import { Button } from 'antd-mobile';
-import { Field } from '@formily/react';
 
 const form = createForm<{}>();
 
@@ -11,11 +10,11 @@ const Component: FunctionComponent = () => {
   if (!FormItem.BaseItem) {
     return null;
   }
+
   return (
     <Form form={form}>
       <Form.Header>标题1</Form.Header>
-      <Field name="username" title="用户名" required decorator={[FormItem]} component={[Input]} />
-      <Field name="password" title="密码" required decorator={[FormItem]} component={[Input]} />
+
       <Button
         onClick={async () => {
           const data = await form.submit();
